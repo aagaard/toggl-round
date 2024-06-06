8baa1cb234c217501b5ad3834f65bc5e2afa6ae8
Søren Siersted Mikkelsen <soren@aamikkelsen.dk>
2024-06-06 13:46:03 +0200

    refactor(run.sh): update script to use poetry
    - Switch from virtualenv to poetry for environment management.
    - Ensure the script runs from the project root directory.

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-source /root/.local/share/virtualenvs/toggl-round-ixjOdO2R/bin/postactivate
-/root/.local/share/virtualenvs/toggl-round-ixjOdO2R/bin/python /root/src/toggl-round/togglRound.py
+# Run poetry from the project root directory
+pushd $(dirname $0)/
+poetry run python time_entry_rounding.py
+popd
